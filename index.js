356ca28729898c59ff310d6e05bf8d4672ce4708
Sunny <sunny.heyar@gmail.com>
2021-09-14 23:47:11 -0400

    added cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", async (req, res) => {
   try {
