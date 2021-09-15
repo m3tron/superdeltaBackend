14aba896ae10d9e844c2657ebd2df7c3e225c2f1
Sunny <sunny.heyar@gmail.com>
2021-09-14 23:38:52 -0400

    added error checking to request

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", async (req, res) => {
-  const URL = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${process.env.GOOGLE_PLACEID}&fields=reviews&key=${process.env.GOOGLE_APIKEY}`;
+  try {
+    const URL = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${process.env.GOOGLE_PLACEID}&fields=reviews&key=${process.env.GOOGLE_APIKEY}`;
 
-  const { data } = await axios.get(URL);
-  res.send(data);
+    const { data } = await axios.get(URL);
+    res.send(data);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
